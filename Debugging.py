9cfbd28ed054722f96e3f1ca4a0149a2747e99ef
shaikhirfanabbas <irfanabbasshaikh93@gmail.com>
2019-01-04 11:50:26 +0530

    first commit

---
+++ Debugging.py
@@ -0,0 +1,7 @@
+import pdb
+pdb.set_trace()
+name=input('Please type your name:')
+age=input('Please type your age :')
+print('hello {} your age is {}'.format(name,age))
+age1=int(age)+5
+print('{} you will be {} in next five year'.format(name,age1))
